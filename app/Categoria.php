5c0d29ac84d20ca89432418a050f767b24f4d1f2
SOLRAK77 <ccastillo.perez77@gmail.com>
2018-06-26 01:23:31 -0500

    Lecciones 22,23 creacion de la tabla con migrate y creacion del modelo

---
+++ app/Categoria.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    //protected $table = 'categorias';
+    //protected $primarykey = 'id';
+
+    //con la propiedad fillable, se indica los campos de la tabla
+    protected $fillable = ['nombre','descripcion','condicion'];
+}
