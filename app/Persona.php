ff7525a7038fbe30cc88f819f6e7827eb8823a0c
SOLRAK77 <ccastillo.perez77@gmail.com>
2018-07-08 07:52:26 -0500

    Lec 46-51 Pantalla cliente - tabla personas Creacion de la tabla, modelo, controlador Cracion de la pantalla de mantenimiento, Alta-Cambio

---
+++ app/Persona.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    //
+    protected $fillable = [
+        'nombre','tipo_documento','num_documento','direccion','telefono','email'
+    ];
+}
